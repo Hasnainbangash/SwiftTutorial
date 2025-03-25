07b2d7c3b697985b46f3ec28490de7f922442ec8
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 13:07:49 +0500

    Initial Commit

---
+++ SwiftTutorial/SwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTutorialApp.swift
+//  SwiftTutorial
+//
+//  Created by Elexoft on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
