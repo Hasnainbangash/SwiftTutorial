3bc6396d63c9c63e57a75883093420efa8e894d3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 14:27:06 +0500

    Added the offset to the label and the roundedrectangle of color gray to make the button look like a 3d button

--- SwiftTutorial/ThreeDButton.swift
+++ SwiftTutorial/ThreeDButton.swift
@@ -10,14 +10,19 @@ import SwiftUI
 struct ThreeDButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         ZStack {
+            
+            let offset: CGFloat = 5
+            
             RoundedRectangle(cornerRadius: 6)
                 .foregroundColor(.black)
-                .offset(y: 5)
+                .offset(y: offset)
             
             RoundedRectangle(cornerRadius: 6)
                 .foregroundColor(.gray)
+                .offset(y: configuration.isPressed ? offset : 0)
             
             configuration.label
+                .offset(y: configuration.isPressed ? offset : 0)
         } //: ZSTACK
     }
 }
